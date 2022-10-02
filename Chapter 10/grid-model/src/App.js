ea2a936b01b7ca0b0944229cf24e2de32084a5fa
Nissim Ram <nissimram1812@gmail.com>
2022-10-01 22:23:46 -0400

    html and css set up for homepage

---
+++ Chapter 10/grid-model/src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <section className="homepage-wrapper">
+        <div className="top-row">
+          <p className="square"> Arc 1</p>
+          <p className="square"> Arc 2</p>
+        </div>
+        <div className="bottom-row">
+          <p className="square"> Arc 3</p>
+          <p className="square"> Arc 4</p>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default App;
