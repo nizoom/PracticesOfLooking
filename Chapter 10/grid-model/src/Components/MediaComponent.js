4fbc6cfb580abd202276bf8e3d4519a1b86fcaa2
Nissim Ram <nissimram1812@gmail.com>
2022-10-03 18:00:04 -0400

    improved img loading time using useEffect hook

--- Chapter 10/grid-model/src/Components/MediaComponent.js
+++ Chapter 10/grid-model/src/Components/MediaComponent.js
@@ -1,16 +1,17 @@
-import React from "react";
-import ChapterPointOneImage1 from "../Chapters/10.1/image1.png";
+import React, { useEffect, useState } from "react";
 
 const MediaComponent = (props) => {
+  const [img, setImg] = useState();
+
+  useEffect(() => {
+    const imageSrc = require(`../Chapters/${props.subChapter}/image${props.page}.png`);
+    setImg(imageSrc);
+  });
+
   return (
     <div className="media-component-wrapper">
       <div className="image-wrapper">
-        <img
-          className="image-square"
-          src={require(`../Chapters/${props.subChapter}/image${props.page}.png`)}
-          // height="450"
-          // width="450"
-        />
+        <img className="image-square" src={img} />
       </div>
     </div>
   );
